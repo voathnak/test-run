0ab6a348f395bbf8120f8751c753ff7751132117
voathnak <voathnak.lim@gmail.com>
2014-11-18 08:10:27 +0700

    add new file

--- Tkinter_test_2.py
+++ Tkinter_test_2.py
@@ -1,17 +1,50 @@
 import Tkinter as tk
-import ttk
+import tkFileDialog 
+import tkMessageBox
 
-class Example(tk.Tk):
+class App(tk.Tk):
     def __init__(self):
         tk.Tk.__init__(self)
         self.floater = FloatingWindow(self)
+        self.floater2 = FloatingWindow(self)
 
 class FloatingWindow(tk.Toplevel):
     def __init__(self, *args, **kwargs):
-        # tk.Toplevel.__init__(self, *args, **kwargs)
-        pass
+        tk.Toplevel.__init__(self, *args, **kwargs)
         self.overrideredirect(True)
         self.wm_geometry("400x400")
+        # self.wm_geometry("0x0")
 
-app=Example()
+        self.label = tk.Label(self, text="Click on the grip to move")
+        self.grip = tk.Label(self, bitmap="gray25")
+        self.grip.pack(side="left", fill="y")
+        self.label.pack(side="right", fill="both", expand=True)
+
+        B = tk.Button(self, text ="Hello", command = self.helloCallBack)
+        B.pack()
+
+        self.grip.bind("<ButtonPress-1>", self.StartMove)
+        self.grip.bind("<ButtonRelease-1>", self.StopMove)
+        self.grip.bind("<B1-Motion>", self.OnMotion)
+
+    def StartMove(self, event):
+        self.x = event.x
+        self.y = event.y
+
+    def StopMove(self, event):
+        self.x = None
+        self.y = None
+
+    def OnMotion(self, event):
+        deltax = event.x - self.x
+        deltay = event.y - self.y
+        x = self.winfo_x() + deltax
+        y = self.winfo_y() + deltay
+        self.geometry("+%s+%s" % (x, y))
+    def helloCallBack(self):
+        tkMessageBox.showinfo( "Hello Python", "Hello World")
+
+
+
+app=App()
 app.mainloop()
