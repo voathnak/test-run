7ead953849efdef2e765d4f266fe81d2ca1a2285
voathnak <voathnak.lim@gmail.com>
2014-11-18 08:02:50 +0700

    add new files

---
+++ pyQT.py
@@ -0,0 +1,28 @@
+from PyQt4 import QtGui, uic
+import sys
+
+# replace 'c:/test.ui' with real path to ui-file created in QtDesigner
+
+import os
+
+uifile = os.path.dirname(__file__) + '/test.ui'
+print uifile
+form, base = uic.loadUiType(uifile)
+
+
+class testQtWindow(QtGui.QMainWindow):
+    def __init__(self, parent=None):
+        QtGui.QWidget.__init__(self, parent)
+        self.ui = form()
+        self.ui.setupUi(self)
+
+
+def main():
+    app = QtGui.QApplication(sys.argv)
+    myapp = testQtWindow()
+    myapp.show()
+    sys.exit(app.exec_())
+
+
+if __name__=="__main__":
+    main()
